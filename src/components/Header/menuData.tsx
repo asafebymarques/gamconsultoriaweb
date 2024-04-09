778f6a5c5f8f6d99c6cc6ae4ada3a3749f6109d7
Asafe Marques <asafebymarques@icloud.com>
2024-04-09 19:14:05 -0300

    First Commit

---
+++ src/components/Header/menuData.tsx
@@ -0,0 +1,29 @@
+import { Menu } from "@/types/menu";
+
+const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/",
+    newTab: false,
+  },
+  {
+    id: 2,
+    title: "Sobre",
+    path: "/about",
+    newTab: false,
+  },
+  {
+    id: 33,
+    title: "Blog",
+    path: "/blog",
+    newTab: false,
+  },
+  {
+    id: 3,
+    title: "Contato",
+    path: "/contact",
+    newTab: false,
+  }
+];
+export default menuData;
