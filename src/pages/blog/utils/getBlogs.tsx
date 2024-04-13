85a776347c8003fa6261eb9e1bf36fab29185bb6
Asafe Marques <asafebymarques@icloud.com>
2024-04-13 11:25:19 -0300

    fixed

--- src/pages/blog/utils/getBlogs.tsx
+++ src/pages/blog/utils/getBlogs.tsx
@@ -1,6 +1,6 @@
 export async function getBlogs() {
-    const response = await fetch(`https://66195447125e9bb9f299b6c0.mockapi.io/blogs`);
-    const blogsData = await response.json();
+    //const response = await fetch(`https://66195447125e9bb9f299b6c0.mockapi.io/blogs`);
+    const blogsData = [];
 
     return blogsData;
 }
